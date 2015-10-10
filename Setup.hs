0e635ba99f32ad816a44e7affade2fdee3b7b936
RobertZK <technoguyrob@gmail.com>
2015-10-10 18:40:24 -0500

    Interim progress on FFI wrapper

--- Setup.hs
+++ Setup.hs
@@ -1,11 +1,15 @@
 import Distribution.Simple
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils (rawSystemExit)
+import System.Environment (lookupEnv, setEnv)
 
 main = defaultMainWithHooks simpleUserHooks
     { preBuild = \a b -> makeLib a b >> preBuild simpleUserHooks a b }
 
 makeLib :: Args -> BuildFlags -> IO ()
-makeLib _ flags =
-    rawSystemExit (fromFlag $ buildVerbosity flags) "env"
-        ["CFLAGS=-D_LIB", "make", "--directory=abc", "libxgboost.a"]
+makeLib _ flags = do
+  let verbosity = fromFlag $ buildVerbosity flags
+  cflags <- lookupEnv "CFLAGS" >>= return . maybe "" id
+  setEnv "CFLAGS" $ "-fPIC -D_LIB" ++ (' ' : cflags)
+  rawSystemExit verbosity "env"
+      ["make", "--directory=xgboost", "libxgboostwrapper.so"]
