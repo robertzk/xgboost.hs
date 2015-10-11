6a36fa810232a58e5e959b4d8eb6e25c88f0f34c
RobertZK <technoguyrob@gmail.com>
2015-10-11 13:52:09 -0500

    Fix typo in CreateFromCST method name.

--- src/Xgboost/Foreign.hs
+++ src/Xgboost/Foreign.hs
@@ -18,7 +18,7 @@ foreign import ccall "XGBGetLastError"
   xgboostGetLastError :: CString -> IO CInt
 
 foreign import ccall "XGDMatrixCreateFromCSR"
-  xgboostMatrixCreateFromFile :: (Ptr CULong) -> (Ptr CUInt) -> (Ptr CFloat) -> CULong -> CULong -> (Ptr DMatrixHandle) -> IO CInt
+  xgboostMatrixCreateFromCSR :: (Ptr CULong) -> (Ptr CUInt) -> (Ptr CFloat) -> CULong -> CULong -> (Ptr DMatrixHandle) -> IO CInt
 
 foreign import ccall "XGDMatrixCreateFromFile"
   xgboostMatrixCreateFromFile :: (Ptr CFloat) -> CInt -> CULong -> CFloat -> (Ptr DMatrixHandle) -> IO CInt
