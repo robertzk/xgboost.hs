5c515457d156a9dd82e6159e1837be93eb84cb70
RobertZK <technoguyrob@gmail.com>
2015-10-11 10:33:18 -0500

    Add trivial testing infrastructure

---
+++ tests/Test.hs
@@ -0,0 +1,14 @@
+
+main :: IO ()
+main = do
+  check_trivial
+
+-- Stolen: https://github.com/haskell/pretty/blob/master/tests/Test.hs
+myAssert :: String -> Bool -> IO ()
+myAssert msg b = putStrLn $ (if b then "Ok, passed " else "Failed test:\n  ") ++ msg
+
+check_trivial = do
+  putStrLn " = Trivial Tests = "
+  myAssert "Trivial test" True
+
+
